c57efcb4711cff848725b448170bfcfb60e8dbe1
Billy <bc35786@gmail.com>
2024-07-11 19:33:41 -0500

    Add exercises completed on desktop

--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -1,3 +1,105 @@
-export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
+const dir = [
+    [-1, 0], // left
+    [1, 0], // right
+    [0, -1], // down
+    [0, 1], // up
+];
 
+function walk(
+    maze: string[],
+    wall: string,
+    curr: Point,
+    end: Point,
+    seen: boolean[][],
+    path: Point[],
+): boolean {
+    // 1. Base case
+    // off the map
+    if (
+        curr.x < 0 ||
+        curr.x >= maze[0].length ||
+        curr.y < 0 ||
+        curr.y >= maze.length
+    ) {
+        return false;
+    }
+
+    // on a wall
+    if (maze[curr.y][curr.x] === wall) {
+        return false;
+    }
+
+    // at the end
+    if (curr.x === end.x && curr.y === end.y) {
+        path.push(end);
+        return true;
+    }
+
+    if (seen[curr.y][curr.x]) {
+        return false;
+    }
+
+    // 3 recurse
+    // pre
+    seen[curr.y][curr.x] = true;
+    path.push(curr);
+
+    // recurse
+    for (let i = 0; i < dir.length; i++) {
+        const [x, y] = dir[i];
+        if (
+            walk(
+                maze,
+                wall,
+                {
+                    x: curr.x + x,
+                    y: curr.y + y,
+                },
+                end,
+                seen,
+                path,
+            )
+        ) {
+            return true;
+        }
+    }
+
+    // post
+
+    path.pop();
+
+    return false;
 }
+
+export default function solve(
+    maze: string[],
+    wall: string,
+    start: Point,
+    end: Point,
+): Point[] {
+    const seen: boolean[][] = [];
+    const path: Point[] = [];
+
+    for (let i = 0; i < maze.length; ++i) {
+        seen.push(new Array(maze[0].length).fill(false));
+    }
+
+    walk(maze, wall, start, end, seen, path);
+
+    return path;
+}
+
+const maze = [
+    "xxxxxxxxxx*x",
+    "x        x*x",
+    "x        x*x",
+    "x xxxxxxxx*x",
+    "x**********x",
+    "x*xxxxxxxxxx",
+];
+const wall = "x";
+const start = { x: 10, y: 0 }; // Starting at the top-left corner (S)
+const end = { x: 1, y: 5 }; // Starting at the top-left corner (S)
+
+const path = solve(maze, wall, start, end);
+console.log(path);
--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,42 @@
 export default class Stack<T> {
     public length: number;
-
-    
+    private head?: ListNode<T>;
 
     constructor() {
+        this.head = undefined;
+        this.length = 0;
     }
 
     push(item: T): void {
+        const node = { value: item } as ListNode<T>;
+        this.length++;
+        if (!this.head) {
+            this.head = node;
+            return;
+        }
 
-}
+        // imagine a linked list reversed (o <= o <= o)
+        // node.prev is the new node added at the very right side
+        // pointing to the soon to be old head
+        node.prev = this.head;
+
+        // point head at new node (head)
+        this.head = node;
+    }
     pop(): T | undefined {
+        this.length = Math.max(0, this.length - 1);
+        if (this.length === 0) {
+            const head = this.head;
+            this.head = undefined;
+            return head?.value;
+        }
 
-}
+        const head = this.head as ListNode<T>;
+        this.head = head.prev;
+
+        return head.value;
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
