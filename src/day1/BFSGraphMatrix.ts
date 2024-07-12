347bf62fcde677ca11a5bc37b162121bcf309592
Billy <bc35786@gmail.com>
2024-07-11 20:39:57 -0500

    Complete BFSGraphMatrix exercise

--- src/day1/BFSGraphMatrix.ts
+++ src/day1/BFSGraphMatrix.ts
@@ -1,3 +1,68 @@
-export default function bfs(graph: WeightedAdjacencyMatrix, source: number, needle: number): number[] | null {
+/* import { matrix2 } from "__tests__/graph";
+ */
+export const matrix2: WeightedAdjacencyMatrix = [
+    [0, 3, 1, 0, 0, 0, 0], // 0
+    [0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 7, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 5, 0, 2, 0],
+    [0, 0, 18, 0, 0, 0, 1],
+    [0, 0, 0, 1, 0, 0, 1],
+];
 
+type WeightedAdjacencyMatrix = number[][]; // A number means weight
+
+export default function bfs(
+    graph: WeightedAdjacencyMatrix,
+    source: number,
+    needle: number,
+): number[] | null {
+    const seen = new Array(graph.length).fill(false);
+
+    const prev = new Array(graph.length).fill(-1);
+
+    seen[source] = true;
+    const q: number[] = [source];
+
+    do {
+        const curr = q.shift() as number;
+
+        if (curr === needle) {
+            break;
+        }
+
+        const adjs = graph[curr];
+        for (let i = 0; i < graph.length; i++) {
+            if (adjs[i] === 0) {
+                continue;
+            }
+
+            if (seen[i]) {
+                continue;
+            }
+
+            seen[i] = true;
+            prev[i] = curr;
+            q.push(i);
+        }
+        seen[curr] = true;
+    } while (q.length);
+
+    if (prev[needle] === -1) {
+        return null;
+    }
+
+    // build it backwards
+
+    let curr = needle;
+    const out: number[] = [];
+
+    while (prev[curr] !== -1) {
+        out.push(curr);
+        curr = prev[curr];
+    }
+
+    return [source].concat(out.reverse());
 }
+
+bfs(matrix2, 6, 0);
